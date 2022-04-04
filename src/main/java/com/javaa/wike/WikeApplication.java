10b40e1bc1ec6f85ce257cce7525d222c3a5eeb8
overton <overton@weipaidang.net>
2022-04-04 15:11:45 +0800

    2.1

--- src/main/java/com/javaa/wike/WikeApplication.java
+++ src/main/java/com/javaa/wike/WikeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WikeApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(WikeApplication.class, args);
     }
