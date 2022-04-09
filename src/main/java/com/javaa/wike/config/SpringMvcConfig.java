8b1b045ab20f784d424a7e7d8c9cbe5a75878479
overton <overton@weipaidang.net>
2022-04-09 23:44:14 +0800

    5.7

---
+++ src/main/java/com/javaa/wike/config/SpringMvcConfig.java
@@ -0,0 +1,34 @@
+package com.javaa.wike.config;
+
+
+import com.javaa.wike.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LogInterceptor loginInterceptor;
+
+
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/test/**"
+//                        "/redis/**",
+//                        "/user/login",
+//                        "/category/all",
+//                        "/ebook/list",
+//                        "/doc/all/**",
+//                        "/doc/vote/**",
+//                        "/doc/find-content/**",
+//                        "/ebook-snapshot/**"
+                );
+    }
+}
